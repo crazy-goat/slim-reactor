8942e2faa84fa5607009f519b9ccbb042fdf491f
Piotr Hałas <piotr@halas.net.pl>
2018-02-25 18:43:31 +0100

    Simplify example

--- examples/slim-http-server.php
+++ examples/slim-http-server.php
@@ -6,12 +6,11 @@ use Slim\{App, Http\Request, Http\Response};
 
 include '../vendor/autoload.php';
 
-$app = new App();
-$app->get('/', function (Request $request, Response $response, $args) {
+$slimReactor  = new SlimReactor();
+$slimReactor->getApp()->get('/', function (Request $request, Response $response, $args) {
     return $response->write('Hello World!');
 });
 
-$slimReactor  = new SlimReactor($app);
 echo 'Server is listening on socket: '.$slimReactor->getSocket()->getAddress()."\n";
 
 $slimReactor->run();
